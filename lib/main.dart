75bbbda91845c5dcd7ab99b16ad18e901a165a0e
mohamed-eltwahy <mohammed.s.eltwahy@gmail.com>
2026-04-23 18:57:28 +0200

    refactor(main): remove debug-only bad certs handling
    Eliminate the conditional check for allowing bad HTTPS certificates in the main function, simplifying the HTTP overrides setup for the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/app/app_body.dart';
 import 'core/bloc/bloc_observer.dart';
 import 'core/injection/injection_container.dart' as injection;
 import 'dart:io';
-
-// Debug-only escape hatch for misconfigured HTTPS certificates.
-// Enable ONLY for local dev:
-// `flutter run --dart-define=ALLOW_BAD_CERTS=true`
-const bool _allowBadCerts =
-    bool.fromEnvironment('ALLOW_BAD_CERTS', defaultValue: false);
 
 class _DebugHttpOverrides extends HttpOverrides {
   @override
@@ -24,9 +17,7 @@ class _DebugHttpOverrides extends HttpOverrides {
 }
 
 void main() async {
-  if (kDebugMode && _allowBadCerts) {
-    HttpOverrides.global = _DebugHttpOverrides();
-  }
+  HttpOverrides.global = _DebugHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   await injection.init();
